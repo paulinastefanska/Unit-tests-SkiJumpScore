0fd0736d25cccbb3ada4701636cb3f0ffb92a1d4
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-17 14:20:31 +0200

    Add function distance tests

---
+++ calculateDistancePoints.test.js
@@ -0,0 +1,34 @@
+const assert = require('assert');
+const calculateDistancePoints = require('./calculateDistancePoints');
+
+    describe('calculateDistancePoints', () => {
+        it('should return normal hillSize points', () => {
+            const actual = calculateDistancePoints(102.5, 109, 98);
+
+            const expected = 69;
+
+            assert.equal(actual, expected);
+        });
+        it('should return high hillSize points', () => {
+            const actual = calculateDistancePoints(125, 134, 120);
+
+            const expected = 69;
+
+            assert.equal(actual, expected);
+        });
+        it('should return mammoth hillSize points', () => {
+            const actual = calculateDistancePoints(225, 225, 200);
+
+            const expected = 150;
+
+            assert.equal(actual, expected);
+        });
+        it('should return 0 if value is error', () => {
+            const actual = calculateDistancePoints(-215, 225, 200);
+
+            const expected = 0;
+
+            assert.equal(actual, expected);
+        });
+
+    });
